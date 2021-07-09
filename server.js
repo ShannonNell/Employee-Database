1ca339c97f99bb2ae394b6be84e81c0ed0b95c2d
Shannon <shannon.nell92@gmail.com>
2021-07-09 08:21:47 -0400

    Add department and add role

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require('./db/connection');
 const conTable = require('console.table');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 console.log('Listening on port 3001.');
@@ -9,7 +10,7 @@ console.log('Listening on port 3001.');
 
 // ============= View all Department names and department ids ============ //
 function viewAllDept() {
-    db.query(`SELECT * FROM department;`, function(err, res) {
+    db.query(`SELECT * FROM department;`, function (err, res) {
         if (err) throw err;
         console.table(res);
     });
@@ -19,11 +20,11 @@ function viewAllDept() {
 function viewAllRoles() {
     db.query(`SELECT role.*, department.name AS Department
     FROM role 
-    LEFT JOIN department ON role.department_id = department.id;`, 
-    function(err, res) {
-        if (err) throw err;
-        console.table(res);
-    });
+    LEFT JOIN department ON role.department_id = department.id;`,
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
 };
 
 // ============= View all Employees: emp ids, first, last name, job title, departments, salaries, managers that emps report to ============ //
@@ -37,26 +38,80 @@ function viewAllEmployees() {
     ON department.id = role.department_id 
     LEFT JOIN employees e 
     ON employees.manager_id = e.id;`,
-    function(err, res) {
-        if (err) throw err;
-        console.table(res);
-    });
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
 };
 
 
 // ============= Add a Department ============ //
+function addDept() {
 
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What Department would you like to add?"
+        }
+    ]).then(function (res) {
+        var query = db.query(
+            `INSERT INTO department (name) VALUES (?)`,
+            {
+                name: res.name
+            },
+            function (err) {
+                if (err) throw err;
+                console.table(res);
+            }
+        )
+    });
+};
 
-
-
+// // ============= Add Employee Role ============ //
+function addRole() {
+    db.query(`SELECT role.title 
+    AS title, role.salary 
+    AS Salary 
+    FROM role`, function (err, res) {
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the employee role title you'd like to add?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the Salary?"
+            },
+            {
+                name: "department_id",
+                type: "input",
+                message: "What is the department ID number?"
+            }
+        ]).then(function (res) {
+            db.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: res.title,
+                    salary: res.salary,
+                    department_id: res.department_id,
+                },
+                function (err) {
+                    if (err) throw err
+                    console.table(res);
+                }
+            )
+        });
+    });
+}
 
 // =============  ============ //
-// =============  ============ //
-
-
-
 
 
 // viewAllDept();
 // viewAllRoles();
-viewAllEmployees();
+// viewAllEmployees();
+// addDept();
+addRole();
